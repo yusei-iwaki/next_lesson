529799aac23e223b8022f2b13780c7635fe9b98c
Yuusei.iwaki <iwaki-yu@iwaki-yu.>
2025-03-20 16:19:54 +0900

    エラー修正

--- app/api/purchases/[userId]/route.ts
+++ app/api/purchases/[userId]/route.ts
@@ -2,8 +2,8 @@ import { NextResponse } from "next/server";
 import prisma from "@/app/lib/prisma";
 
 // 購入履歴検索用のAPI
-export async function GET(req: Request, context: {params: {userId: string}}) {
-    const {userId} = context.params;
+export async function GET(req: Request, {params}: {params: {userId: string}}) {
+    const {userId} = params;
     try {
         const purchases = await prisma.purchase.findMany({
             where: {
